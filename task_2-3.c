3b54b20f50e98f96e0e8e9c19021e795c7595956
barsegyansamvel <barsegyan.pnz@gmail.com>
2026-09-25 17:34:48 +0300

    Update task_2-3.c

--- task_2-3.c
+++ task_2-3.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /*
+@brief Запрашивает у пользователя стороны кирпича (x, y, z)
+@param x Указатель на переменную для первой стороны
+@param y Указатель на переменную для второй стороны
+@param z Указатель на переменную для третьей стороны
+*/
+void vvod_kirpich(double* x, double* y, double* z);
+
+/*
+@brief Запрашивает у пользователя стороны отверстия (r, s)
+@param r Указатель на переменную для первой стороны
+@param s Указатель на переменную для второй стороны
+*/
+void vvod_otverstie(double* r, double* s);
+
+/*
 @brief Проверяет, помещается ли грань со сторонами a и b в отверстие r x s
 @param a Первая сторона грани
 @param b Вторая сторона грани
@@ -9,7 +24,7 @@
 @param s Вторая сторона отверстия
 @return 1, если помещается, 0 - если нет
 */
-int proverka(double a, double b, double r, double s);
+int proverka(const double a, const double b, const double r, const double s);
 
 /*
 @brief Точка входа в программу
@@ -17,66 +32,48 @@ int proverka(double a, double b, double r, double s);
 */
 int main(void)
 {
-    double x, y, z;
-    double r, s;
+    double x=0, y=0, z=0;
+    double r=0, s=0;
 
-    printf("Vvedite storony kirpicha (x y z): ");
-    if (scanf("%lf %lf %lf", &x, &y, &z) != 3) {
-        printf("Oshibka vvoda!\n");
-        return -1;
-    }
+    input_kirpich(&x, &y, &z);
+    input_otverstie(&r, &s);
 
-    printf("Vvedite storony otverstiya (r s): ");
-    if (scanf("%lf %lf", &r, &s) != 2) {
-        printf("Oshibka vvoda!\n");
-        return -1;
-    }
-
-    int result = 0;
-
-    if (proverka(x, y, r, s))
+    if (proverka(x, y, r, s) || proverka(x, z, r, s) || proverka(y, z, r, s))
     {
-        result = 1;
-    }
-    else if (proverka(x, z, r, s))
-    {
-        result = 1;
-    }
-    else if (proverka(y, z, r, s))
-    {
-        result = 1;
-    }
-
-
-    if (result == 1)
-    {
-        printf("Kirpich PROYDET skvoz' otverstie.\n");
+        printf("\nKirpich PROYDET skvoz' otverstie.\n");
     }
     else
     {
-        printf("Kirpich NE proydet skvoz' otverstie.\n");
+        printf("\nKirpich NE proydet skvoz' otverstie.\n");
     }
-
     return 0;
 }
 
-int proverka(double a, double b, double r, double s)
+void vvod_kirpich(double* x, double* y, double* z)
 {
-    if (a <= r)
+    printf("Vvedite storony kirpicha (x y z): ");
+    if (scanf("%lf %lf %lf", x, y, z) != 3)
     {
-        if (b <= s)
-        {
-            return 1;
-        }
+        printf("Oshibka vvoda!\n");
+        exit(EXIT_FAILURE);
     }
+}
 
-    if (a <= s)
+void vvod_otverstie(double* r, double* s)
+{
+    printf("Vvedite storony otverstiya (r s): ");
+    if (scanf("%lf %lf", r, s) != 2)
     {
-        if (b <= r)
-        {
-            return 1;
-        }
+        printf("Oshibka vvoda!\n");
+        exit(EXIT_FAILURE);
     }
+}
 
+int fits(double a, double b, double r, double s)
+{
+    if ((a <= r && b <= s) || (a <= s && b <= r))
+    {
+        return 1;
+    }
     return 0;
 }
